74cd83f142a83ae61585f7764b01bbb4e13794b8
Elin <51740831+EllVEBIT@users.noreply.github.com>
2020-10-06 13:15:37 +0500

    Update colors.sh

--- .termux/colors.sh
+++ .termux/colors.sh
@@ -3,7 +3,7 @@ DIR=`cd $(dirname $0); pwd`
 COLORS_DIR=$DIR/colors
 count=0
 
-echo -e "The default color theme is Tango.\nYou can choose another one from the list below";
+echo -e "Цветовая тема по умолчанию Tango.\nВы можете выбрать другой из списка";
 
 for colors in "$COLORS_DIR"/*; do
   colors_name[count]=$( echo $colors | awk -F'/' '{print $NF}' )
@@ -13,17 +13,17 @@ done;
 count=$(( $count - 1 ));
 
 while true; do
-  read -p 'Enter a number, leave blank to not to change:' number;
+  read -p 'Введите номер, оставьте поле пустым, чтобы не менять:' number;
   if [[ -z "$number" ]]; then
     break;
   elif ! [[ $number =~ ^[0-9]+$ ]]; then
-    echo "Please enter the right number!";
+    echo "Пожалуйста, введите правильный номер!";
   elif (( $number>=0 && $number<=$count )); then
     eval choice=${colors_name[number]};
     cp -fr "$COLORS_DIR/$choice" "$DIR/colors.properties";
     break;
   else
-    echo "Please enter the right number!";
+    echo "Пожалуйста, введите правильный номер!";
   fi
 done
 
